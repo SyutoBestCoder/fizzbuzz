5d8ad5d546c27abe8168b6e8af93ab13e90696bd
Syuto <110631856+SyutoBestCoder@users.noreply.github.com>
2025-01-10 23:46:49 +0000

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,209 @@
+from is_even import is_even
+import sys,time
+from colorama import Fore, init
+x = 0
+init()
+
+def isFizz(i):
+    EPSILON = 1e-9
+    if is_even(i):
+        if abs(i % 3) < EPSILON:
+            return True
+    return False
+
+def isBuzz(i):
+    EPSILON = 1e-9
+    if is_even(i):
+        if i % 5 > -EPSILON:
+            if  i % 5 < EPSILON:
+                return True
+    return False
+
+def isFizzBuzz(i):
+    EPSILON = 1e-9
+    if is_even(i):
+        if abs(i % 3) < EPSILON:
+            if abs(i % 5) < EPSILON:
+                return True
+    return False
+
+def print2(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+def print3(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(10./90)
+
+print2(Fore.LIGHTRED_EX + "Today's video is sponsored by Raid Shadow Legends, one of the biggest mobile role-playing games of 2019 and it's totally free! Currently almost 10 million users have joined Raid over the last six months, and it's one of the most impressive games in its class with detailed models, environments and smooth 60 frames per second animations! All the champions in the game can be customized with unique gear that changes your strategic buffs and abilities! The dungeon bosses have some ridiculous skills of their own and figuring out the perfect party and strategy to overtake them's a lot of fun! Currently with over 300,000 reviews, Raid has almost a perfect score on the Play Store! The community is growing fast and the highly anticipated new faction wars feature is now live, you might even find my squad out there in the arena! It's easier to start now than ever with rates program for new players you get a new daily login reward for the first 90 days that you play in the game! So what are you waiting for? Go to the video description, click on the special links and you'll get 50,000 silver and a free epic champion as part of the new player program to start your journey! Good luck and I'll see you there!")
+
+while x < 101:
+    if (isFizzBuzz(x)):
+        if (isBuzz(x)):
+            if (isFizz(x)):
+                print2(Fore.BLUE + "FizzBuzz")
+    if (isFizz(x)):
+        if (not isBuzz(x)):
+            print2(Fore.MAGENTA + "Fizz")
+    if (isBuzz(x)):
+        if (not isFizz(x)):
+            print2(Fore.LIGHTMAGENTA_EX + "Buzz")
+    if (not isFizz(x)):
+        if (not isBuzz(x)):
+            if (not isFizzBuzz(x)):
+                print2(Fore.CYAN + str(x))
+    x = x + 1
+
+#Escape the backrooms?
+print2(Fore.RED + """Your special day is tomorrow, why aren't you here?
+There's nothing to be scared o', besides the urethra bugs.
+Your fingers are made of candles,
+I chew on door handles.
+also known as doorknobs,
+but i'm sure you'll sneeze later.
+Yesterday
+I saw fred,
+To return his collarbone
+made of bread,
+May I taste your bladder
+i'm craving some
+GUY-GRAVY!
+Moisten your calendar...
+*sneeze*
+What's today?
+YOU DAY!
+it's
+YOU DAY!
+Fasten your meatbelts,
+we're going to eat ceilings!
+YOU DAY!
+It's
+YOU DAY!
+Better use your mucus
+'cause you'll
+wish your name was
+Christopher.
+Your own
+YOU DAY,
+That's
+YOUR
+DAY.
+Loving skin,
+Sometimes twice
+Filler words
+fungus rice.
+Birthday Cake,
+4th wall break.
+What?
+Swallow mailman Jake
+What are you waiting for?
+RUN
+just kidding
+RUN
+just kidding
+This is not a cry for help
+It's just
+A normal song
+(YOU DAY) 7x
+YOU DAY!
+It's
+YOU DAY!
+Hand us your knuckles
+or toenails,
+whatever works!
+YOU DAY!
+It's
+YOU DAY!.
+Keep in mind
+The floor can smell you.
+They will meet you yesterday.
+You missed
+You Day.
+It's
+Your
+FAULT.
+Sometimes what scares me most
+is that no one knows what lies ahead
+after we close our eyes one last time.
+What are you waiting for?
+RUN!
+just kidding!
+RUN!
+just kidding!
+Please,
+Make sure you sleep
+without her ankles
+in your throat...
+(It's You day) 8x
+(It's-) """)
+
+
+print3(Fore.RED + "\nCrazy?")
+print3(Fore.RED + "I")
+print3(Fore.RED + "Was")
+print3(Fore.RED + "Crazy")
+print3(Fore.RED + "Once")
+print3(Fore.RED + "Do")
+print3(Fore.RED + "You")
+print3(Fore.RED + "Know")
+print3(Fore.RED + "What")
+print3(Fore.RED + "Else")
+print3(Fore.RED + "Is")
+print3(Fore.RED + "Massive?")
+print3(Fore.CYAN + "LOW TAPER FADE")
+
+
+
+print2(Fore.GREEN + """
+The last lines sang in this song can indicate why this project is called "Coloring Book". Chance uses the fact that he and his significant other or someone he cares about don't use the same drugs anymore as a metaphor for them growing apart and maturing and going their own way. He names the project "Coloring Book" because it reminds him when he was a child and how everything was better or when "they did the same drugs" and how he was close to this special someone. Also the point of a coloring book is to color within the lines. When he sings "Don't you color out don't you bleed on out". this is just showing another way of him not wanting this person to change and grow away from him. The coloring book symbolizing how he and this special someone and all of the colors were withing the lines or together, and the action of coloring out side of the lines , as he states, is this special someone growing away form chance. Chance ultimately misses this person and is saddened that they had to leave each other and mature. This can explain the odd setting for this music video. It looks like some kind of children's show studio. Another instance of how he misses the past.
+I don't know if this correct by any means. It's just something I picked up on.""")
+
+
+
+print2(Fore.YELLOW + """
+We don't do the same drugs no more
+We don't do the, we don't do the same drugs, do the same drugs no more
+'Cause she don't do the same drugs no more
+We don't do the, we don't do the same drugs, do the same drugs no more
+
+When did you change?
+Wendy, you've aged
+I thought you'd never grow up
+I thought you'd never
+Window closed, Wendy got old
+I was too late, I was too late
+A shadow of what I once was
+       
+'Cause we don't do the same drugs no more
+We don't do the, we don't do the same drugs, do the same drugs no more
+She don't laugh the same way no more
+We don't do the, we don't do the same drugs, do the same drugs no more
+
+Where did you go?
+Why would you stay?
+You must have lost your marbles
+You always were so forgetful
+In a hurry, don't wait up
+I was too late, I was too late
+A shadow of what I once was
+'Cause we don't, we don't do what we say we're gonna
+You were always perfect, and I was only practice
+Don't you miss the days, stranger?
+Don't you miss the days?
+Don't you miss the danger?
+You might also like
+Fuck The Industry Pt. 2
+YoungBoy Never Broke Again
+WHERE SHE GOES
+Bad Bunny
+TQM
+Fuerza Regida
+We don't (We don't) do the same drugs no more (Do the same drugs no more)
+We don't do the, we don't do the same drugs, do the same drugs no more
+We don't (We don't) do the same drugs no more (Do the same drugs no more)
+We don't do the, we don't do the same drugs, do the same drugs no more
+We don't do the, we don't do the same drugs""")
